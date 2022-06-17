9ca90f8b9682a5d7f1125db6ded341be3ea0a126
ganesh <ganeshkirdak26@gmail.com>
2022-06-17 11:54:13 +0530

    terraform

--- 03-terraform-count/ec2instance.tf
+++ 03-terraform-count/ec2instance.tf
@@ -4,8 +4,8 @@ resource "aws_instance" "amazon" {
   user_data = file("${path.module}/app1-install.sh")
   key_name = var.key_name
   vpc_security_group_ids = [ aws_security_group.vpc-ssh.id , aws_security_group.vpc-http.id]
-  instance_type = var.instance_type_list[1]  # For List
-  instance_type = var.instance_type_map["prod"]  # For Map
+  #instance_type = var.instance_type_list[1]  # For List
+  #instance_type = var.instance_type_map["prod"]  # For Map
   count = 2
   tags = {
     "Name" = "Count-Demo-${count.index}"
--- 04-terraform-for-each/ec2instance.tf
+++ 04-terraform-for-each/ec2instance.tf
@@ -1,14 +1,25 @@
+
+# Availability Zones Datasource
+data "aws_availability_zones" "my_azones" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+
+# EC2 instance
 resource "aws_instance" "amazon" {
   ami = data.aws_ami.amzlinux2.id
   instance_type = var.instance_type
   user_data = file("${path.module}/app1-install.sh")
   key_name = var.key_name
   vpc_security_group_ids = [ aws_security_group.vpc-ssh.id , aws_security_group.vpc-http.id]
-  instance_type = var.instance_type_list[1]  # For List
-  instance_type = var.instance_type_map["prod"]  # For Map
-  count = 2
+ # Create EC2 Instance in all Availabilty Zones of a VPC  
+  for_each = toset(data.aws_availability_zones.my_azones.names)
+  availability_zone = each.key  # You can also use each.value because for list items each.key == each.value
   tags = {
-    "Name" = "Count-Demo-${count.index}"
+    "Name" = "for_each-Demo-${each.value}"
   }
 }
 
