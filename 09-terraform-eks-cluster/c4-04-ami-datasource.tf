5786fd946c9b1663d2223b872a41890152da61c5
ganesh <ganeshkirdak26@gmail.com>
2022-06-21 19:48:35 +0530

    main

---
+++ 09-terraform-eks-cluster/c4-04-ami-datasource.tf
@@ -0,0 +1,21 @@
+# Get latest AMI ID for Amazon Linux2 OS
+data "aws_ami" "amzlinux2" {
+  most_recent = true
+  owners = [ "amazon" ]
+  filter {
+    name = "name"
+    values = [ "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20220610" ]
+  }
+  filter {
+    name = "root-device-type"
+    values = [ "ebs" ]
+  }
+  filter {
+    name = "virtualization-type"
+    values = [ "hvm" ]
+  }
+  filter {
+    name = "architecture"
+    values = [ "x86_64" ]
+  }
+}
